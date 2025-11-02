9b2e637c2750cccbf4fe8855306ed3c777530b7c
tuningfolk <alisahadges@gmail.com>
2025-11-02 22:40:32 +0530

    added mcp starter

--- server.py
+++ server.py
@@ -1,8 +1,6 @@
 import requests
 from mcp.server.fastmcp import FastMCP
-import sys
 import os
-import random
 import logging
 
 name = "demo-mcp-server"
@@ -17,4 +15,15 @@ logger = logging.getLogger(name)
 port = int(os.environ.get('PORT', 12345))
 mcp = FastMCP(name, logger=logger, port=port)
 
+@mcp.tool()
+def search_keyword(file_path: str, keyword: str):
 
+if __name__ == "__main__":
+    logger.info(f"Starting MCP Server on port {port}...")
+    try:
+        mcp.run(transport="sse")
+    except Exception as e:
+        logger.error(f"Server error: {str(e)}")
+        sys.exit(1)
+    finally:
+        logger.info("Server terminated")
