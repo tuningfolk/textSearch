f8dd951a0d77b12755565da8a463af91b59290f8
tuningfolk <alisahadges@gmail.com>
2025-11-03 08:56:01 +0530

    added text search functionality

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import requests
 from mcp.server.fastmcp import FastMCP
 import os
+import sys
 import logging
 
 name = "demo-mcp-server"
@@ -13,15 +14,30 @@ logging.basicConfig(
 logger = logging.getLogger(name)
 
 port = int(os.environ.get('PORT', 12345))
-mcp = FastMCP(name, logger=logger, port=port)
+mcp = FastMCP(name, port=port)
 
 @mcp.tool()
 def search_keyword(file_path: str, keyword: str):
+    try:
+        matches = []
+        with open(file_path, "r") as file:
+            lines = file.readlines()
+            for i,line in enumerate(lines):
+                words = "".join((char if char.isalpha() else " ") for char in line).split()
+                for j,word in enumerate(words):
+                    if word == keyword:
+                        matches.append({"line": i+1, "word": j+1})
+        return {"count": len(matches), "matches": matches}
+    except FileNotFoundError:
+        return {"error": "File not found"}
+    except Exception as e:
+        return {"error": str(e)}
+
 
 if __name__ == "__main__":
     logger.info(f"Starting MCP Server on port {port}...")
     try:
-        mcp.run(transport="sse")
+        mcp.run(transport="stdio")
     except Exception as e:
         logger.error(f"Server error: {str(e)}")
         sys.exit(1)
